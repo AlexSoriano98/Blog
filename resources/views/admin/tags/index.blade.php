9e4b3132b507e3c25c4436111016ab9a0a3a6cb1
Wolyo <alex.soriano9825@gmail.com>
2021-05-05 13:11:10 -0600

    Mostrar botones por roles

--- resources/views/admin/tags/index.blade.php
+++ resources/views/admin/tags/index.blade.php
@@ -3,7 +3,9 @@
 @section('title', 'Coderss')
 
 @section('content_header')
+    @can('admin.tags.create')
     <a class="btn btn-secondary btn-sm float-right" href="{{route('admin.tags.create')}}">Nueva etiquetas</a>
+    @endcan    
     <h1>Lista de etiquetas</h1>
 @stop
 
@@ -33,14 +35,18 @@
                             <td>{{$tag->name}}</td>
                             <td>{{$tag->color}}</td>
                             <td width="10px">
-                                <a class="btn btn-primary btn-sm" href="{{route('admin.tags.edit', $tag)}}">Editar</a>
+                                @can('admin.tags.edit')
+                                    <a class="btn btn-primary btn-sm" href="{{route('admin.tags.edit', $tag)}}">Editar</a>
+                                @endcan
                             </td>
                             <td width="10px">
-                                <form action="{{route('admin.tags.destroy',$tag)}}" method="POST">
-                                    @csrf
-                                    @method('delete')
-                                    <button type="submit" class="btn btn-danger btn-sm">Eliminar</button>
-                                </form>
+                                @can('admin.tags.destroy')
+                                    <form action="{{route('admin.tags.destroy',$tag)}}" method="POST">
+                                        @csrf
+                                        @method('delete')
+                                        <button type="submit" class="btn btn-danger btn-sm">Eliminar</button>
+                                    </form>
+                                @endcan
                             </td>
                         </tr>
                     @endforeach
